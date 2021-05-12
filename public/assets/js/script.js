2f041d9f5fde9b0bb67d0346de6d98bbf8cb55e5
HEMMER <79737617+HEMMER-FX@users.noreply.github.com>
2021-05-12 18:12:14 +0200

    Delete public directory

--- public/assets/js/script.js
+++
@@ -1,4 +0,0 @@
-let cupcakeList = document.getElementById('cupcake-list');
-let cupcakeItems = document.getElementsByClassName('cupcake-item');
-let nbCupcake = cupcakeItems.length;
-cupcakeList.style.height = nbCupcake * 85 + 'px';
