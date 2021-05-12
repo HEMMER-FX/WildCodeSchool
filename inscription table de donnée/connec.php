949091eef2dbe4df3604300a196f1f3209b92f11
HEMMER <79737617+HEMMER-FX@users.noreply.github.com>
2021-05-12 18:06:47 +0200

    Update connec.php

--- inscription table de donnée/connec.php
+++ inscription table de donnée/connec.php
@@ -1,8 +1,8 @@
 <?php
 // Définition des constantes(identifiants) lié à ma BDD localhost
-define('DSN','mysql:host=localhost;dbname=032021_kaamelott');
-define('USER','yavuz');
-define('PASS','yavuz');
+define('DSN','mysql:host=localhost;dbname=namebdd');
+define('USER','nameuser');
+define('PASS','passuser');
 
 // Cette ligne permet la connexion à la BDD
 $pdo = new PDO(DSN, USER, PASS);
